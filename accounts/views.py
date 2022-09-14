5b09cb1682957b0950e73f519e557c3fb98766dc
alienperfect <alienperfecthq@gmail.com>
2022-09-14 16:36:30 +0300

    reset migrations, remove doctring, create index page

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import views
-from django.http.response import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 
 class LoginView(views.LoginView):
     template_name = 'accounts/login.html'
-    next_page = '/'
+    next_page = reverse_lazy('main')
     redirect_authenticated_user = True
