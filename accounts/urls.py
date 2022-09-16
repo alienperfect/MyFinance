73c0e431b7885a520e1a1a3b8b46a07e43d37dd4
alienperfect <alienperfecthq@gmail.com>
2022-09-16 19:21:39 +0300

    add links

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from accounts.views import LoginView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
