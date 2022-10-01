dc0b9e81aa32ef81cc31a50903887f6af238ff5b
alienperfect <alienperfecthq@gmail.com>
2022-10-01 18:54:06 +0300

    add get_arrow() in table_sort.py. now arrows are displayed properly

--- accounting/templatetags/table_sort.py
+++ accounting/templatetags/table_sort.py
@@ -5,17 +5,28 @@ from collections import OrderedDict
 register = template.Library()
 
 @register.simple_tag
-def url_replace(request, field, value, direction=''):
-    dict_ = request.GET.copy()
+def order_by(request, field, value, direction=''):
+    request_copy = request.GET.copy()
 
-    if field == 'order_by' and field in dict_.keys():          
-        if dict_[field].startswith('-') and dict_[field].lstrip('-') == value:
-            dict_[field] = value
-        elif dict_[field].lstrip('-') == value:
+    if field == 'order_by' and field in request_copy.keys():          
+        if request_copy[field].startswith('-') and request_copy[field].lstrip('-') == value:
+            request_copy[field] = value
+        elif request_copy[field].lstrip('-') == value:
             return ''
         else:
-            dict_[field] = direction + value
+            request_copy[field] = direction + value
     else:
-        dict_[field] = direction + value
+        request_copy[field] = direction + value
+    return urlencode(OrderedDict(sorted(request_copy.items())))
 
-    return urlencode(OrderedDict(sorted(dict_.items())))
+@register.simple_tag
+def get_arrow(request, key):
+    value = request.GET.get('order_by', '')
+    filename = 'sort'
+
+    if value and key in value:
+        if value.startswith('-'):
+            filename = 'sort-down'
+        else:
+            filename = 'sort-up'
+    return f'http://127.0.0.1:8000/static/img/{filename}.svg'
