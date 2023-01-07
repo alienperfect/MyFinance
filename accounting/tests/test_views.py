da2d541bb7a84e223fe0ce1b36a182eadb431675
alienperfect <alienperfecthq@gmail.com>
2023-01-07 13:23:01 +0200

    make tests for RelatedCategoryMixinTestCase, SingleObjectSearchMixinTestCase; remove unnecessary tests

--- accounting/tests/test_views.py
+++ accounting/tests/test_views.py
@@ -3,48 +3,226 @@ from django.urls import reverse_lazy
 
 from accounts.models import User
 from accounting.models import Category, ExpensesUnit, IncomeUnit
-from accounting.views import ExpensesUnitCreateView
+from accounting.views import ExpensesUnitCreateView, RelatedCategoryMixin, SingleObjectSearchMixin
 from accounting.forms import ExpensesUnitForm
 
 
-class ExpensesUnitCreateViewTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls) -> None:
-        cls.client = Client()
-        cls.factory = RequestFactory()
-        cls.user = User.objects.create_user(email='test@email.com', username='test', password='test')
-
-    def test_response_code_is_200(self):
-        self.client.force_login(self.user)
-        resp = self.client.get(reverse_lazy('accounting:expenses-unit-create'))
-
-        self.assertEqual(resp.status_code, 200)
-
-    def test_template_used(self):
-        self.client.force_login(self.user)
-        resp = self.client.get(reverse_lazy('accounting:expenses-unit-create'))
-
-        self.assertTemplateUsed(resp, 'accounting/unit_create.html')
-
+class RelatedCategoryMixinTestCase(TestCase):
+    def setUp(self) -> None:
+        self.rf = RequestFactory()
+    
     def test_post_ajax(self):
-        self.client.force_login(self.user)
-
-        resp = self.client.post(
+        request = self.rf.post(
             reverse_lazy('accounting:expenses-unit-create'),
-            {'name': 'test'},
+            {'name': 'category'},
             HTTP_X_Requested_With='XMLHttpRequest',
             )
 
-        cat = Category.objects.get(id=1)
-
-        self.assertJSONEqual(str(resp.content, encoding='utf-8'), {'id': cat.id})
-
-    def test_form_valid_with_categories(self):
-        form = ExpensesUnitForm({'name': 'test', 'quantity': 1, 'price': 100, 'purchase_date': '2022-12-27'})
-        cat = Category.objects.create(name='test')
-        view = ExpensesUnitCreateView(categories=[cat])
-        view.categories
-        view.form_valid(form)
+        mixin = RelatedCategoryMixin()
+        resp = mixin.post(request)
         
-        e = ExpensesUnit.objects.get(id=1)
-        print(e.categories.all())
+        self.assertJSONEqual(str(resp.content, encoding='utf8'), {'id': 1})
+
+
+class SingleObjectSearchMixinTestCase(TestCase):
+    def setUp(self) -> None:
+        self.rf = RequestFactory()
+    
+    def test_get_queryset_request_data_empty(self):
+        expected = [
+            ExpensesUnit.objects.create(
+                name=f'expenses{i}',
+                quantity=1,
+                price=2,
+                purchase_date='2023-01-04',
+                ) for i in range(2)
+            ]
+
+        mixin = SingleObjectSearchMixin()
+        mixin.model = ExpensesUnit
+
+        mixin.filters = {
+            'name': 'name__icontains',
+            'quantity': 'quantity',
+            'created': 'created__date',
+            'categories': 'categories__name__in',
+            'total_price': 'total_price',
+            'total_income': 'total_income',
+            'purchase_date': 'purchase_date',
+            'receive_date': 'receive_date',
+            }
+        
+        mixin.ordering = '-created'
+
+        mixin.request = self.rf.get(reverse_lazy('accounting:unit-list'))
+        queryset = mixin.get_queryset()
+        queryset = [queryset[i] for i in range(len(queryset))]
+ 
+        self.assertEqual(queryset, expected)
+
+    def test_get_queryset_key_model(self):
+        expected = [
+            ExpensesUnit.objects.create(
+                name='expenses',
+                quantity=1,
+                price=2,
+                purchase_date='2023-01-04',
+                ),
+            ]
+        
+        IncomeUnit.objects.create(
+                name='income',
+                quantity=1,
+                income=2,
+                receive_date='2023-01-04',
+                )
+        
+        mixin = SingleObjectSearchMixin()
+        mixin.model_dict = {'expenses': ExpensesUnit, 'income': IncomeUnit}
+
+        mixin.filters = {
+            'name': 'name__icontains',
+            'quantity': 'quantity',
+            'created': 'created__date',
+            'categories': 'categories__name__in',
+            'total_price': 'total_price',
+            'total_income': 'total_income',
+            'purchase_date': 'purchase_date',
+            'receive_date': 'receive_date',
+            }
+        
+        mixin.ordering = '-created'
+
+        mixin.request = self.rf.get(
+            reverse_lazy('accounting:unit-list'),
+            {'model': 'expenses'},
+            )
+        
+        queryset = mixin.get_queryset()
+        queryset = [queryset[i] for i in range(len(queryset))]
+
+        self.assertEqual(queryset, expected)
+
+    def test_get_queryset_key_order_by(self):
+        expected = [
+            ExpensesUnit.objects.create(
+                name=f'{i}expenses',
+                quantity=1,
+                price=2,
+                purchase_date='2023-01-04',
+                ) for i in range(2)
+            ]
+        
+        mixin = SingleObjectSearchMixin()
+        mixin.model = ExpensesUnit
+
+        mixin.filters = {
+            'name': 'name__icontains',
+            'quantity': 'quantity',
+            'created': 'created__date',
+            'categories': 'categories__name__in',
+            'total_price': 'total_price',
+            'total_income': 'total_income',
+            'purchase_date': 'purchase_date',
+            'receive_date': 'receive_date',
+            }
+        
+        mixin.ordering = '-created'
+
+        mixin.request = self.rf.get(
+            reverse_lazy('accounting:unit-list'),
+            {'order_by': 'name'},
+            )
+        
+        queryset = mixin.get_queryset()
+        queryset = [queryset[i] for i in range(len(queryset))]
+
+        self.assertEqual(queryset, expected)
+
+    def test_get_queryset_key_in(self):
+        expected = [
+            ExpensesUnit.objects.create(
+                name='expenses0',
+                quantity=1,
+                price=2,
+                purchase_date='2023-01-04',
+                )
+            ]
+        
+        ExpensesUnit.objects.create(
+                name='expenses1',
+                quantity=1,
+                price=2,
+                purchase_date='2023-01-04',
+                )
+        
+        Category.objects.create(name='cat').expenses_unit.add(expected[0])
+
+        mixin = SingleObjectSearchMixin()
+        mixin.model = ExpensesUnit
+
+        mixin.filters = {
+            'name': 'name__icontains',
+            'quantity': 'quantity',
+            'created': 'created__date',
+            'categories': 'categories__name__in',
+            'total_price': 'total_price',
+            'total_income': 'total_income',
+            'purchase_date': 'purchase_date',
+            'receive_date': 'receive_date',
+            }
+        
+        mixin.ordering = 'name'
+
+        mixin.request = self.rf.get(
+            reverse_lazy('accounting:unit-list'),
+            {'categories': 'cat'},
+            )
+        
+        queryset = mixin.get_queryset()
+        queryset = [queryset[i] for i in range(len(queryset))]
+
+        self.assertEqual(queryset, expected)
+    
+    def test_get_queryset_other_keys(self):
+        expected = [
+            ExpensesUnit.objects.create(
+                name='expenses0',
+                quantity=1,
+                price=2,
+                purchase_date='2023-01-04',
+                )
+            ]
+        
+        ExpensesUnit.objects.create(
+                name='expenses1',
+                quantity=1,
+                price=2,
+                purchase_date='2023-01-04',
+                )
+
+        mixin = SingleObjectSearchMixin()
+        mixin.model = ExpensesUnit
+
+        mixin.filters = {
+            'name': 'name__icontains',
+            'quantity': 'quantity',
+            'created': 'created__date',
+            'categories': 'categories__name__in',
+            'total_price': 'total_price',
+            'total_income': 'total_income',
+            'purchase_date': 'purchase_date',
+            'receive_date': 'receive_date',
+            }
+        
+        mixin.ordering = '-created'
+
+        mixin.request = self.rf.get(
+            reverse_lazy('accounting:unit-list'),
+            {'name': 'expenses0'},
+            )
+        
+        queryset = mixin.get_queryset()
+        queryset = [queryset[i] for i in range(len(queryset))]
+
+        self.assertEqual(queryset, expected)
